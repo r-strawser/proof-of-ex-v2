1d4dfe9a1a922f814ee1eb3271d0aab90e718a9c
r-strawser <strawser.ryan@gmail.com>
2021-01-11 14:42:24 -0800

    Redefine routing in main app shell

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -17,8 +17,8 @@ import PendingTransactionsView  from 'containers/PendingTransactionsView'
 import LatestUploadsView        from 'containers/LatestUploadsView'
 import UploadView               from 'containers/UploadView'
 import RegisterView             from 'containers/RegisterView'
-import Header                   from './components/Header'
-import Footer                   from './components/Footer'
+import NormalLayoutRoute        from './layouts/NormalLayoutRoute'
+import RegistrationLayoutRoute  from './layouts/RegistrationLayoutRoute'
 import MetaMaskNotification     from './components/MetaMaskNotification'
 
 import './styles.scss' // global styles
@@ -35,17 +35,14 @@ class App extends Component {
       <MuiThemeProvider theme={theme}>
         <HashRouter>
           <div>
-            <Header />
-            <Footer />
             <div className="app-shell">
               <Switch>
-                <Route path="/home" component={HomeView} />
-                <Route path="/assets" component={AssetsView} />
-                <Route path="/pending" component={PendingTransactionsView} />
-                <Route path="/latest" component={LatestUploadsView} />
-                <Route path="/upload" component={UploadView} />
-                <Route path="/register" component={RegisterView} />
-                <Redirect from="/" to="/home" />
+                <NormalLayoutRoute path="/assets" component={AssetsView} />
+                <NormalLayoutRoute path="/pending" component={PendingTransactionsView} />
+                <NormalLayoutRoute path="/latest" component={LatestUploadsView} />
+                <NormalLayoutRoute path="/upload" component={UploadView} />
+                <RegistrationLayoutRoute path="/register" component={RegisterView} />
+                <Redirect from="/" to="/assets" />
               </Switch>
             </div>
           </div>
