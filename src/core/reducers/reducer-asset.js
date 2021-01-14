c351fdcd033b6dc58e4ddf538ac3559e195c9780
r-strawser <strawser.ryan@gmail.com>
2021-01-13 16:09:10 -0800

    updated fix for not pushing through redux

--- src/core/reducers/reducer-asset.js
+++ src/core/reducers/reducer-asset.js
@@ -1,36 +1,36 @@
 import constants from 'core/types'
 
-
 const initialState = {
-    stagedAsset: null,
-    assetHash: '',
-    alreadyExists: false,
-    error: '',
-    transaction: null,
-    success: false
-  }
+  stagedAsset: null,
+  assetHash: '',
+  alreadyExists: false,
+  error: '',
+  transaction: null,
+  success: false
+}
 
 export function assetReducer(state = initialState, action) {
-    switch (action.type) {
-        case constants.ADD_ASSET:
-            return Object.assign({}, state, {
-            stagedAsset: action.asset[0]
-        })
+  switch (action.type) {
 
-        case constants.CHECK_ASSET:
-            return Object.assign({}, state, {
-            assetHash: action.assetHash,
-            alreadyExists: action.alreadyExists
-        })
+  case constants.ADD_ASSET:
+    return Object.assign({}, state, {
+      stagedAsset: action.asset[0]
+    })
 
-        case constants.CREATE_ASSET_HASH:
-            return Object.assign({}, state, {
-            assetHash: action.hash,
-            success: action.success,
-            transaction: action.transaction
-        })
+  case constants.CREATE_ASSET_HASH:
+    return Object.assign({}, state, {
+      assetHash: action.hash,
+      success: action.success,
+      transaction: action.transaction
+    })
 
-        default:
-            return state
-    }
+  case constants.CHECK_ASSET:
+    return Object.assign({}, state, {
+      assetHash: action.assetHash,
+      alreadyExists: action.alreadyExists
+    })
+
+  default:
+    return state
+  }
 }
